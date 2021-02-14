d7bc9745787cc493eaa99255a03ca84e96d8d10f
juanluna96 <juandluna17@gmail.com>
2021-02-13 21:29:22 -0500

    Creando Formulario de Gastos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Pregunta from './components/Pregunta'
+import Formulario from './components/Formulario'
 
 function App() {
   // Definir el state
@@ -8,11 +9,19 @@ function App() {
 
 
   return (
-    <div className="container mx-auto">
+    <div className="container px-3 mx-auto md:px-0">
       <header>
         <h1 className="my-10 text-5xl text-center text-white">Gasto semanal</h1>
         <div className="py-5 mx-auto bg-white rounded shadow-md md:max-w-3xl">
           <Pregunta setPresupuesto={ setPresupuesto } setRestante={ setRestante }></Pregunta>
+          <div className="flex-row p-3 mt-3 md:flex">
+            <div className="md:w-1/2">
+              <Formulario></Formulario>
+            </div>
+            <div className="md:w-1/2">
+              2
+            </div>
+          </div>
         </div>
       </header>
     </div>
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Formulario = () => {
+    return (
+        <form>
+            <h2 className="mb-5 italic font-bold text-center text-blue-500">Agrega tus datos aquí</h2>
+
+            <label htmlFor="gasto" className="font-bold">Nombre gasto:</label>
+            <input
+                type="text"
+                placeholder="Ej. transporte"
+                id="gasto"
+                className="w-full px-3 py-2 mt-2 mb-5 leading-tight text-gray-700 border rounded shadow-lg appearance-none focus:outline-none focus:shadow-outline"
+            />
+
+            <label htmlFor="cantidad_gasto" className="font-bold">Cantidad gasto:</label>
+            <input
+                type="number"
+                placeholder="Ej. 300"
+                id="cantidad_gasto"
+                className="w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow-lg appearance-none focus:outline-none focus:shadow-outline"
+            />
+
+            <input type="submit" value="Agregar gasto" className="block w-full py-3 mx-auto mt-5 font-bold text-white bg-red-600 rounded hover:bg-red-dark" />
+        </form>
+    )
+}
+
+export default Formulario
