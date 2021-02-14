85c68a292751891d89d3b8d6d6e17f353c200579
juanluna96 <juandluna17@gmail.com>
2021-02-13 22:48:21 -0500

    Crear un nuevo gasto

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Error from './Error';
+import shortid from 'shortid';
 
 const Formulario = () => {
     const [nombre, setNombre] = useState('');
@@ -31,6 +32,13 @@ const Formulario = () => {
         setError(false);
 
         //Construir el gasto
+        const gasto = {
+            nombre,
+            cantidad,
+            id: shortid.generate()
+        }
+
+        console.log(gasto)
 
         // Pasar el gasto al componente principal
 
